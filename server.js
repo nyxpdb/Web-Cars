fcad0c7869a3a3c35b5a08fdc9f74b663efd8218
Nyx <ana01cardoso02@gmail.com>
2025-05-12 20:50:10 -0300

    refactor: improve server setup in server.js

--- server.js
+++ server.js
@@ -1,51 +1,43 @@
-// Load environment variables from the .env file
 require('dotenv').config();
-
-// Import necessary dependencies
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express'); 
+const swaggerSpec = require('./docs/swagger'); 
 const carRoutes = require('./routes/carRoutes');
 
-// Initialize the Express app
 const app = express();
 
-// Middleware setup
-app.use(cors()); // Enable Cross-Origin Resource Sharing (CORS)
-app.use(bodyParser.json()); // Parse incoming JSON requests
+app.use(cors());
+app.use(bodyParser.json());
 
-// Set up the routes for the API
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api/cars', carRoutes);
 
-// Function to connect to MongoDB database
 const connectToDatabase = async () => {
   try {
-    // Establish a connection to MongoDB
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('Database connection established successfully.');
+    console.log('Conectado ao MongoDB');
   } catch (err) {
-    console.error('Error connecting to the database:', err.message);
-    process.exit(1); // Exit the application if database connection fails
+    console.error('Erro na conexão com o MongoDB:', err.message);
+    process.exit(1);
   }
 };
 
-// Function to start the server
 const startServer = (port) => {
   app.listen(port, () => {
-    console.log(`Server is running on port ${port}.`);
+    console.log(`Servidor rodando na porta ${port}`);
   });
 };
 
-// Initialize the app: Connect to the database and then start the server
 const initializeApp = async () => {
-  await connectToDatabase(); // Connect to the database
-  const PORT = process.env.PORT || 3000; // Use the environment variable or default to port 3000
-  startServer(PORT); // Start the server
+  await connectToDatabase();
+  const PORT = process.env.PORT || 3000;
+  startServer(PORT);
 };
 
-// Run the initialization function
 initializeApp();
